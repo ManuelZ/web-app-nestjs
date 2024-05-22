21029b61c9d7d978c34158ed4a26a9d220dd6430
ManuelZ <115771+ManuelZ@users.noreply.github.com>
2024-05-22 14:33:32 -0500

    Add port argument to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.BACKEND_PORT || 3000;
+  console.log(
+    `Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`,
+  );
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
