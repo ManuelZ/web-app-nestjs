68345cba35cf42facadcc1cfee31f46c4db58721
ManuelZ <115771+ManuelZ@users.noreply.github.com>
2024-05-25 19:02:57 -0500

    Add CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   console.log(
     `Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`,
   );
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(port);
 }
 bootstrap();
